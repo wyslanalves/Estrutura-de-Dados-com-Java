b10bac6a7db64d2b80c0cd49e795e9d1b9fba168
wyslan alves <wyslandasilvaalves@gmail.com>
2022-07-21 15:48:01 -0300

    Adicionando Elemento no Vetor

---
+++ estruturadados-algoritmos-java/src/com/wyslan/estrututadados/vetor/teste/Aula02.java
@@ -0,0 +1,15 @@
+package com.wyslan.estrututadados.vetor.teste;
+
+import com.wyslan.estrututadados.vetor.Vetor;
+
+public class Aula02 {
+	public static void main(String[] args) {
+
+		
+		Vetor vetor = new Vetor(5);
+		
+		int i = 0;
+		i++;
+	}
+
+}
---
+++ estruturadados-algoritmos-java/src/com/wyslan/estrututadados/vetor/teste/Aula03.java
@@ -0,0 +1,16 @@
+package com.wyslan.estrututadados.vetor.teste;
+
+import com.wyslan.estrututadados.vetor.Vetor;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(2);
+			
+			vetor.adiciona("elemento 1");
+			vetor.adiciona("elemento 2");
+			vetor.adiciona("elemento 3");
+		
+	}
+}
